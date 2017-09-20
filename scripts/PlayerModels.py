a108d12d73b57be3f40bf651d4028c8a48072e02
EvgenyKashin <kashinevge@gmail.com>
2017-09-20 23:31:24 +0300

    All simple player models moved in the separate module

---
+++ scripts/PlayerModels.py
@@ -0,0 +1,127 @@
+from pypokerengine.players import BasePokerPlayer
+from pypokerengine.utils.card_utils import gen_cards, estimate_hole_card_win_rate, _montecarlo_simulation
+import random as rand
+
+
+class CallPlayer(BasePokerPlayer):  # Do not forget to make parent class as "BasePokerPlayer"
+    #  we define the logic to make an action through this method. (so this method would be the core of your AI)
+    def declare_action(self, valid_actions, hole_card, round_state):
+        # valid_actions format => [raise_action_info, call_action_info, fold_action_info]
+        call_action_info = valid_actions[1]
+        action, amount = call_action_info["action"], call_action_info["amount"]
+        return action, amount   # action returned here is sent to the poker engine
+
+    def receive_game_start_message(self, game_info):
+        pass
+
+    def receive_round_start_message(self, round_count, hole_card, seats):
+        pass
+
+    def receive_street_start_message(self, street, round_state):
+        pass
+
+    def receive_game_update_message(self, action, round_state):
+        pass
+
+    def receive_round_result_message(self, winners, hand_info, round_state):
+        pass
+
+
+class FoldPlayer(BasePokerPlayer):  # Do not forget to make parent class as "BasePokerPlayer"
+    #  we define the logic to make an action through this method. (so this method would be the core of your AI)
+    def declare_action(self, valid_actions, hole_card, round_state):
+        # valid_actions format => [raise_action_info, call_action_info, fold_action_info]
+        call_action_info = valid_actions[0]
+        action, amount = call_action_info["action"], call_action_info["amount"]
+        return action, amount   # action returned here is sent to the poker engine
+
+    def receive_game_start_message(self, game_info):
+        pass
+
+    def receive_round_start_message(self, round_count, hole_card, seats):
+        pass
+
+    def receive_street_start_message(self, street, round_state):
+        pass
+
+    def receive_game_update_message(self, action, round_state):
+        pass
+
+    def receive_round_result_message(self, winners, hand_info, round_state):
+        pass
+
+    
+class HeuristicPlayer(BasePokerPlayer):  # Do not forget to make parent class as "BasePokerPlayer"
+    #  we define the logic to make an action through this method. (so this method would be the core of your AI)
+    def declare_action(self, valid_actions, hole_card, round_state):
+        # valid_actions format => [raise_action_info, call_action_info, fold_action_info]
+        call_action_info = valid_actions[1]
+        fold_action_info = valid_actions[0]
+
+        community_card = round_state['community_card']
+        win_rate = estimate_hole_card_win_rate(nb_simulation=150, nb_player=self.nb_player,
+                                               hole_card=gen_cards(hole_card),
+                                               community_card=gen_cards(community_card))
+        if win_rate > 1 / float(self.nb_player) + 0.1:
+            action, amount = call_action_info["action"], call_action_info["amount"]
+        else:
+            action, amount = fold_action_info["action"], fold_action_info["amount"]
+        return action, amount
+
+    def receive_game_start_message(self, game_info):
+        self.nb_player = game_info['player_num']
+
+    def receive_round_start_message(self, round_count, hole_card, seats):
+        pass
+
+    def receive_street_start_message(self, street, round_state):
+        pass
+
+    def receive_game_update_message(self, action, round_state):
+        pass
+
+    def receive_round_result_message(self, winners, hand_info, round_state):
+        pass
+
+    
+class RandomPlayer(BasePokerPlayer):
+    def __init__(self):
+        self.fold_ratio, self.call_ratio, raise_ratio = 1.0/5, 3.0/5, 1.0/5
+
+    def set_action_ratio(self, fold_ratio, call_ratio, raise_ratio):
+        ratio = [fold_ratio, call_ratio, raise_ratio]
+        scaled_ratio = [ 1.0 * num / sum(ratio) for num in ratio]
+        self.fold_ratio, self.call_ratio, self.raise_ratio = scaled_ratio
+
+    def declare_action(self, valid_actions, hole_card, round_state):
+        choice = self.__choice_action(valid_actions)
+        action = choice["action"]
+        amount = choice["amount"]
+        if action == "raise":
+            amount = rand.randrange(amount["min"], max(amount["min"], amount["max"]) + 1)
+        return action, amount
+
+    def __choice_action(self, valid_actions):
+        r = rand.random()
+        if r <= self.fold_ratio:
+            return valid_actions[0]
+        elif r <= self.call_ratio:
+            return valid_actions[1]
+        else:
+            return valid_actions[2]
+
+
+    def receive_game_start_message(self, game_info):
+        pass
+
+    def receive_round_start_message(self, round_count, hole_card, seats):
+        pass
+
+    def receive_street_start_message(self, street, round_state):
+        pass
+
+    def receive_game_update_message(self, new_action, round_state):
+        pass
+
+    def receive_round_result_message(self, winners, hand_info, round_state):
+        pass
